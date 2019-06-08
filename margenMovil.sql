0fbd9e403c95d5bfa8d91c78a131e8f2bda0fe1e
Mica Banfi <mbanfi@itba.edu.ar>
2019-06-08 17:45:06 -0300

    message error

--- margenMovil.sql
+++ margenMovil.sql
@@ -2,16 +2,18 @@ CREATE OR REPLACE FUNCTION MargenMovil(fecha DATE,n integer) RETURNS decimal
 as 
 $$
 BEGIN
-
+        IF n = 0 THEN
+                RAISE EXCEPTION 'La cantidad de meses anteriores debe ser mayor a 0';
+        END IF;
         return (select avg(revenue-cost) as margenmovil
-        from (
+          from (
                 select revenue,cost
                 from definitiva        
                 where  sales_date <= fecha and  fecha - (n || 'month')::INTERVAL  <=sales_date
-        ) as subtable);
+          ) as subtable);
 END;
 
 $$ LANGUAGE plpgsql
 
 
-select MargenMovil(to_date('2012-11-01','YYYY-MM-DD'),4);
+select MargenMovil(to_date('2012-11-01','YYYY-MM-DD'),0);
