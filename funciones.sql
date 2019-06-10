7cfbbc586090f4dccd7ab808a1eff10e1d17dfb6
lpinilla <lauti_nipi123@hotmail.com>
2019-06-10 11:06:52 -0300

    arreglado espacios en el print y mergeado todo en funciones.sql

--- funciones.sql
+++ funciones.sql
@@ -1,17 +1,91 @@
--- Reporte de Ventas Histórico
+--                                                                                                             % Tablas
 
-SELECT * FROM definitiva;
+CREATE TABLE intermedia(
+        Quarter TEXT NOT NULL,
+        Month TEXT NOT NULL,
+        Week TEXT NOT NULL,
+        Product_Type TEXT NOT NULL,
+        Territory TEXT NOT NULL,
+        Sales_Channel TEXT NOT NULL,
+        Customer_Type TEXT NOT NULL,
+        Revenue FLOAT NOT NULL,
+        Cost FLOAT NOT NULL,
+        CONSTRAINT non_neg_cost CHECK ( Cost >= 0),
+        PRIMARY KEY(Quarter, Month, Week, Product_Type, Territory, Sales_Channel, Customer_Type)
+);
 
-/* Función para probar si el año es el correcto.
-create or replace function testPrintF(var varchar(100)) returns void as $$
+CREATE TABLE definitiva(
+        id SERIAL,
+        Sales_Date DATE,
+        Product_Type TEXT NOT NULL,
+        Territory TEXT NOT NULL,
+        Sales_Channel TEXT NOT NULL,
+        Customer_Type TEXT NOT NULL,
+        Revenue FLOAT NOT NULL,
+        Cost FLOAT NOT NULL,
+        PRIMARY KEY(id)
+);
+
+CREATE OR REPLACE FUNCTION get_month(month CHAR) RETURNS CHAR AS $$
 BEGIN
-PERFORM DBMS_OUTPUT.DISABLE();
-PERFORM DBMS_OUTPUT.ENABLE();
-PERFORM DBMS_OUTPUT.SERVEROUTPUT('t');
-PERFORM DBMS_OUTPUT.PUT_LINE(var);
+        RETURN(
+                SELECT CASE
+                        WHEN month = 'Jan' THEN '01'
+                        WHEN month = 'Feb' THEN '02'
+                        WHEN month = 'Mar' THEN '03'
+                        WHEN month = 'Apr' THEN '04'
+                        WHEN month = 'May' THEN '05'
+                        WHEN month = 'Jun' THEN '06'
+                        WHEN month = 'Jul' THEN '07'
+                        WHEN month = 'Aug' THEN '08'
+                        WHEN month = 'Sep' THEN '09'
+                        WHEN month = 'Oct' THEN '10'
+                        WHEN month = 'Nov' THEN '11'
+                        WHEN month = 'Dec' THEN '12'
+                END
+        );
 END;
 $$ LANGUAGE plpgsql;
-*/
+
+CREATE OR REPLACE FUNCTION string_to_DATE(year CHAR, month CHAR) RETURNS DATE AS $$
+BEGIN
+        RETURN to_date(year||month||'01');
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION insert_into_definitiva() RETURNS Trigger AS $$
+BEGIN
+        --%insertar en tabla
+        INSERT INTO definitiva VALUES(DEFAULT, string_to_DATE(substr(new.quarter,4,5),get_month(substr(new.month,4,7))) , new.Product_Type, new.Territory, new.Sales_Channel, new.Customer_Type, new.Revenue, new.Cost);        
+        RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;        
+
+--                                                                                                              % Trigger
+
+CREATE TRIGGER InsertaDefinitiva
+BEFORE INSERT ON intermedia
+FOR EACH ROW
+EXECUTE PROCEDURE insert_into_definitiva();
+
+
+--                                                                                                              % Margen movil
+
+CREATE OR REPLACE FUNCTION MargenMovil(fecha DATE,n INTEGER) RETURNS DECIMAL AS $$
+BEGIN
+        IF n = 0 THEN
+                RAISE EXCEPTION 'La cantidad de meses anteriores debe ser mayor a 0';
+        END IF;
+        RETURN (SELECT AVG(revenue-cost) AS margenmovil
+          FROM (
+                SELECT revenue,cost
+                FROM definitiva        
+                WHERE  sales_date <= fecha AND fecha - (n || 'month')::INTERVAL  <=sales_date
+                ) AS subtable);
+END;
+$$ LANGUAGE plpgsql;
+
+--                                                                                                              % Reporte
 
 CREATE OR REPLACE FUNCTION table_titles() RETURNS VOID AS $$
 BEGIN
@@ -21,13 +95,6 @@ PERFORM DBMS_OUTPUT.SERVEROUTPUT ('t');
 PERFORM DBMS_OUTPUT.PUT_LINE ('         HISTORIC SALES REPORT');
 END;
 $$ LANGUAGE plpgsql;
-
-
--- Devuelve el título del reporte.
-select table_titles();
-
-
-
 
 CREATE OR REPLACE FUNCTION obtainDataFromYear(in myYear integer) RETURNS VOID AS $$
 DECLARE
@@ -74,9 +141,6 @@ CLOSE dataByYear;
 END;
 $$ LANGUAGE plpgsql;
 
---% Test de la función para el año 2011. Debería dar todos los datos del año 2011.
-select obtainDataFromYear(2011);
-
 CREATE OR REPLACE FUNCTION ReporteVentas(in n integer) RETURNS VOID AS $$
 DECLARE
 -- Voy a iterar a través de los años.
@@ -96,16 +160,3 @@ FOR list_of_years IN
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
-
-
--- test de la función final
--- con 0 no devuelve nada
--- con 1 devuelve solo el primer año (ASC) que es el 2011
--- con 2 devuelve los dos primeros años (ASC) que son el 2011 y el 2012, en ese orden
--- con 3+ devuelve lo mismo que con 2 porque hay solo 2 años en los datos
-select ReporteVentas(0);
-select ReporteVentas(1);
-select ReporteVentas(2);
-select ReporteVentas(3);
-
-
