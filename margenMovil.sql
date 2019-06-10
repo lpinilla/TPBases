d76f0c558758716f50995279c4127997c1ef06e2
lpinilla <lauti_nipi123@hotmail.com>
2019-06-10 09:46:57 -0300

    corregido ; en margen movil

--- margenMovil.sql
+++ margenMovil.sql
@@ -1,19 +1,16 @@
-CREATE OR REPLACE FUNCTION MargenMovil(fecha DATE,n integer) RETURNS decimal
-as 
-$$
+CREATE OR REPLACE FUNCTION MargenMovil(fecha DATE,n INTEGER) RETURNS DECIMAL AS $$
 BEGIN
         IF n = 0 THEN
                 RAISE EXCEPTION 'La cantidad de meses anteriores debe ser mayor a 0';
         END IF;
-        return (select avg(revenue-cost) as margenmovil
-          from (
-                select revenue,cost
-                from definitiva        
-                where  sales_date <= fecha and  fecha - (n || 'month')::INTERVAL  <=sales_date
-          ) as subtable);
+        RETURN (SELECT AVG(revenue-cost) AS margenmovil
+          FROM (
+                SELECT revenue,cost
+                FROM definitiva        
+                WHERE  sales_date <= fecha AND fecha - (n || 'month')::INTERVAL  <=sales_date
+                ) AS subtable);
 END;
+$$ LANGUAGE plpgsql;
 
-$$ LANGUAGE plpgsql
 
-
-select MargenMovil(to_date('2012-11-01','YYYY-MM-DD'),0);
+select MargenMovil(to_date('2012-11-01','YYYY-MM-DD'),3);
